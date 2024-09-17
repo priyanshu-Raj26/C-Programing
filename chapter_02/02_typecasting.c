40780d1a77bbdb6156e2f30dd0bcc2977db0db88
Priyanshu Raj <er.priyansu@gmail.com>
2024-09-17 15:37:19 +0530

    Add files via upload

---
+++ chapter_02/02_typecasting.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int n = 45;
+    float m = 32.35;
+
+    printf("%d\n", n);
+    n = (int)m; //  convert the tata type into int
+    printf("%d\n", n);
+
+    return 0;
+}
---
+++ chapter_02/03_problem.c
@@ -0,0 +1,8 @@
+// write a program to check weather a number is divisible by 97 or not.
+#include <stdio.h>
+
+int main(){
+    int a=925986664;
+    printf("The value of a%97 is %d",a%97);
+    return 0;
+}
